b4b7487e9b32a9581e13e7b9df1e54571993b88f
sedron <jfabianabadd@gmail.com>
2023-12-25 19:23:46 -0500

    Usado Scaffolding para automatizar la creacion del crud de Alumno

--- Controllers/AlumnoController.cs
+++ Controllers/AlumnoController.cs
@@ -1,59 +1,161 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using ASP_Platzi.Models;
-using ASP_Platzi.Context;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using ASP_Platzi.Context;
+using ASP_Platzi.Models;
 
-namespace ASP_Platzi.Controllers;
-
-public class AlumnoController : Controller
+namespace ASP_Platzi.Controllers
 {
-    private EscuelaContext _context;
-    public AlumnoController(EscuelaContext context)
+    public class AlumnoController : Controller
     {
-        _context = context;
-    }
-    public IActionResult Index()
-    {
-        List<Alumno> listaAlumnos = _context.Alumnos.ToList();
-        
-        return View(listaAlumnos);
-    }
+        private readonly EscuelaContext _context;
 
-    public IActionResult Create()
-    {
-        return View();
-    }
-
-    [HttpPost]
-    public IActionResult Create(Alumno Alumno)
-    {
-        if (ModelState.IsValid)
+        public AlumnoController(EscuelaContext context)
         {
-            _context.Alumnos.Add(Alumno);
-            _context.SaveChanges();
-            return RedirectToAction("Single", "Alumno", new {id=Alumno.Id});
-        } else {
-            return View(Alumno);
+            _context = context;
         }
-    }
 
-    public IActionResult Single(string id)
-    {
-        Alumno alumno = _context.Alumnos.Where(p => p.Id == id).SingleOrDefault();
-        ViewBag.Inscripciones = _context.Inscripciones.Where(p => p.AlumnoId == id).Include(p => p.Curso).ToList();
-        ViewBag.Cursos = _context.Cursos.Include(p => p.Escuela).ToList();
-        return View(alumno);
-    }
-
-    public IActionResult Inscribir(string cursoId, string alumnoId)
-    {
-        _context.Inscripciones.Add(new Inscripcion()
+        // GET: Alumno
+        public async Task<IActionResult> Index()
         {
-            CursoId = cursoId,
-            AlumnoId = alumnoId
-        });
-        _context.SaveChanges();
-        return RedirectToAction("Single", "Alumno", new {id=alumnoId});
-        
+              return View(await _context.Alumnos.ToListAsync());
+        }
+
+        // GET: Alumno/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Alumnos == null)
+            {
+                return NotFound();
+            }
+
+            var alumno = await _context.Alumnos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (alumno == null)
+            {
+                return NotFound();
+            }
+
+            return View(alumno);
+        }
+
+        // GET: Alumno/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Alumno/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre")] Alumno alumno)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(alumno);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(alumno);
+        }
+
+        // GET: Alumno/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Alumnos == null)
+            {
+                return NotFound();
+            }
+
+            var alumno = await _context.Alumnos.FindAsync(id);
+            if (alumno == null)
+            {
+                return NotFound();
+            }
+            return View(alumno);
+        }
+
+        // POST: Alumno/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Nombre")] Alumno alumno)
+        {
+            if (id != alumno.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(alumno);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AlumnoExists(alumno.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(alumno);
+        }
+
+        // GET: Alumno/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Alumnos == null)
+            {
+                return NotFound();
+            }
+
+            var alumno = await _context.Alumnos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (alumno == null)
+            {
+                return NotFound();
+            }
+
+            return View(alumno);
+        }
+
+        // POST: Alumno/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Alumnos == null)
+            {
+                return Problem("Entity set 'EscuelaContext.Alumnos'  is null.");
+            }
+            var alumno = await _context.Alumnos.FindAsync(id);
+            if (alumno != null)
+            {
+                _context.Alumnos.Remove(alumno);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AlumnoExists(string id)
+        {
+          return _context.Alumnos.Any(e => e.Id == id);
+        }
     }
 }
